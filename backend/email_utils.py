758abe0c204908ff40d82b7d6fa325681a669bd9
felipearandia24-hue <felipearandia24@gmail.com>
2026-04-04 00:01:24 -0500

    Add Resend API

--- backend/email_utils.py
+++ backend/email_utils.py
@@ -1,33 +1,22 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
+import resend
 
 EMAIL = "felipearandia24@gmail.com"
 PASSWORD = "aexusudnhvrggzbl"
 
+
+
+resend.api_key = os.getenv("RESEND_API_KEY")
+
 def send_email(to_email, link):
-    print("INTENTANDO ENVIAR EMAIL...")
-
-    try:
-        msg = MIMEText(f"""
-Recupera tu contraseña:
-
-{link}
-""")
-
-        msg["Subject"] = "Recuperar contraseña"
-        msg["From"] = EMAIL
-        msg["To"] = to_email
-
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-
-        server.login(EMAIL, PASSWORD)
-        print("Login OK")
-
-        server.send_message(msg)
-        print("EMAIL ENVIADO")
-
-        server.quit()
-
-    except Exception as e:
-        print("ERROR EMAIL:", e)
+    resend.Emails.send({
+        "from": "onboarding@resend.dev",
+        "to": to_email,
+        "subject": "Recuperar contraseña",
+        "html": f"""
+        <p>Haz clic para recuperar tu contraseña:</p>
+        <a href="{link}">{link}</a>
+        """
+    })
