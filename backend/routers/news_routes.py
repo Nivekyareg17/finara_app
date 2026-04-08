1412f4accccaef158ed83e8a5dd37f4f8144a822
felipearandia24-hue <felipearandia24@gmail.com>
2026-04-08 11:30:49 -0500

    feat: add GNews API KEY

--- backend/routers/news_routes.py
+++ backend/routers/news_routes.py
@@ -8,11 +8,11 @@ news_router = APIRouter(
     tags=["News API"]
 )
 
-API_KEY = os.getenv("FINNHUB_API_KEY")
+API_KEY = os.getenv("GNEWS_API_KEY")
 
 @news_router.get("/")
 def get_news():
-    url = f"https://finnhub.io/api/v1/news?category=general&token={API_KEY}"
+    url = f"https://gnews.io/api/v4/top-headlines?lang=es&topic-business&token={API_KEY}"
 
     response = requests.get(url)
     data = response.json()
@@ -21,11 +21,11 @@ def get_news():
 
     for item in data[:10]:
         noticias.append({
-            "titulo": item.get("headline"),
-            "categoria": item.get("category"),
+            "titulo": item.get("title"),
+            "categoria": "GENERAL",
             "imagen": item.get("image"),
-            "fecha": item.get("datetime"),
-            "fuente": item.get("source"),
+            "fecha": item.get("publishedAt"),
+            "fuente": item.get("source", {}).get("name"),
             "url": item.get("url")
         })
 
