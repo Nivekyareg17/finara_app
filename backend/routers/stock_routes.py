87bc26e755af3eff6d07519ce191f909f8e82801
felipearandia24-hue <felipearandia24@gmail.com>
2026-04-08 20:43:49 -0500

    feat: add routes to stock_market

---
+++ backend/routers/stock_routes.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from fastapi import APIRouter
+
+stock_router = APIRouter(
+    prefix="/api/stocks",
+    tags=["Stocks API"]
+)
+
+API_KEY = os.getenv("FINNHUB_API_KEY")
+
+SYMBOLS = [
+    "AAPL",   # Apple
+    "TSLA",   # Tesla
+    "GOOGL",  # Google
+    "AMZN",   # Amazon
+    "MSFT",   # Microsoft
+    "META",   # Facebook
+    "NVDA",   # Nvidia
+    "NFLX",   # Netflix
+    "AMD",    # AMD
+    "INTC"    # Intel
+]
+
+@stock_router.get("/")
+def get_stocks():
+    stocks = []
+
+    for symbol in SYMBOLS:
+        url = f"https://finnhub.io/api/v1/quote?symbol={symbol}&token={API_KEY}"
+        response = requests.get(url)
+        data = response.json()
+
+        stocks.append({
+            "symbol": symbol,
+            "price": data.get("c"),
+            "change": data.get("d"),
+            "percent": data.get("dp")
+        })
+
+    return stocks
